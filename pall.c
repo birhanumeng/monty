65418fae7c02fa37f16e848c9cbef6abad7f0fdc
birhanumeng <eyerusalemmengiste@gmail.com>
2022-10-06 11:55:53 -0700

    pall

--- pall.c
+++ pall.c
@@ -1,13 +1,13 @@
 #include "monty.h"
 
 /**
- * pall - print all elements on stack
- * @stack: refers to beginning of stack
- * @line_number: line number
+ * m_pall - print all values on `stack' starting from the top
+ * @stack: double pointer to head of stack
+ * @line_number: line number being executed from script file
  *
  * Return: void
  */
-void pall(stack_t **stack, unsigned int line_number)
+void m_pall(stack_t **stack, unsigned int line_number)
 {
 	stack_t *head;
 
