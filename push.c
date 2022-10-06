77bbd8de89dfb1936431e6d389f96559dc488b84
birhanumeng <eyerusalemmengiste@gmail.com>
2022-10-06 11:15:34 -0700

    push

---
+++ push.c
@@ -0,0 +1,51 @@
+#include "monty.h"
+#include <ctype.h>
+
+/**
+ * check_digit - checks a string contains digits
+ * @arg: string
+ *
+ * Return: 0 if only digits, otherwise 1
+ */
+static int check_digit(char *arg)
+{
+	int i;
+
+	for (i = 0; arg[i]; i++)
+	{
+		if (arg[i] == '-' && i == 0)
+			continue;
+		if (isdigit(arg[i]) == 0)
+			return (1);
+	}
+	return (0);
+}
+
+/**
+ * push - push an integer into stack
+ * @stack: refers to beginning of the stack
+ * @line_number: line number
+ *
+ * Return: void
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+	char *arg;
+	int n;
+
+	arg = strtok(NULL, "\n\t\r ");
+	if (arg == NULL || check_for_digit(arg))
+	{
+		dprintf(STDOUT_FILENO,
+			"L%u: usage: push integer\n",
+			line_number);
+		exit(EXIT_FAILURE);
+	}
+	n = atoi(arg);
+	if (!add_node(stack, n))
+	{
+		dprintf(STDOUT_FILENO, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	var.stack_len++;
+}
